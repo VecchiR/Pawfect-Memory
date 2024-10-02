a8bb3cb75007058d51b553bc3005ae434d45780f
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-10-02 11:43:26 -0300

    Store which cards were already clicked

--- src/components/Gameplay/Card.jsx
+++ src/components/Gameplay/Card.jsx
@@ -1,14 +1,7 @@
 import '../../style/Card.css';
 
-export default function Card({imgUrl, breedName, shuffleCards }) {
+export default function Card({imgUrl, breedName, handleCardClick }) {
   
-  const handleCardClick = (e) => {
-    console.log(e.currentTarget);
-    shuffleCards();
-  }
-
-
-
     return (
       <div className="card-container" onClick={handleCardClick}>
         <img src={imgUrl} alt={breedName} />
--- src/components/Gameplay/GameBoard.jsx
+++ src/components/Gameplay/GameBoard.jsx
@@ -7,36 +7,28 @@ import ScoreBoard from './ScoreBoard';
 
 export default function GameBoard({ difficulty = 1, score, bestScore, dogsArr }) {
   const numberOfCards = difficulty === 1 ? 9 : difficulty < 3 ? 16 : 25;
-
   const [cardsToShowArr, setCardsToShowArr] = useState(null);
+  const [shuffledCards, setShuffledCards] = useState(null);
+
   useEffect(() => {
     if (dogsArr) {
       setCardsToShowArr(dogsArr.slice(0, numberOfCards));
+      setShuffledCards(dogsArr.slice(0, numberOfCards));
     }
   }, [dogsArr, numberOfCards]);
-
-  // let cardsToShowArr;
-  // const areCardsReady = () => {
-  //   if (dogsArr) {
-  //     // cardsToShowArr = dogsArr.slice(0, numberOfCards);
-  //     setCardsToShowArr(dogsArr.slice(0, numberOfCards));
-  //     return true;
-  //   }
-  //   return false;
-  // };
 
   const invokeCards = () => {
     return (
       <>
-        {cardsToShowArr &&
-          cardsToShowArr.map((dog, index) => {
+        {shuffledCards &&
+          shuffledCards.map((dog, index) => {
             // console.log('inside the each. Now going through: ', dog);
             return (
               <Card
                 key={index}
                 imgUrl={dog.url}
                 breedName={dog.breed}
-                shuffleCards={shuffleCards}
+                handleCardClick={handleCardClick}
               />
             );
           })}
@@ -44,9 +36,23 @@ export default function GameBoard({ difficulty = 1, score, bestScore, dogsArr })
     );
   };
 
+  const handleCardClick = (e) => {
+    const breed = e.currentTarget.firstChild.alt;
+    const breedIndex = cardsToShowArr.findIndex((dog) => dog.breed === breed);
+    const isClicked = cardsToShowArr[breedIndex].clicked ? true : false;
+    if (breedIndex >= 0) {
+      if (!isClicked) {
+        markCard(breed);
+
+        shuffleCards();
+      } else {
+        alert('this dog was already clicked');
+      }
+    }
+  };
+
   const shuffleCards = () => {
     let copy = [...cardsToShowArr];
-    
 
     //Fisher-Yates algorith - https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=Fisher%2DYates%20algorith
     for (let i = copy.length - 1; i > 0; i--) {
@@ -56,19 +62,23 @@ export default function GameBoard({ difficulty = 1, score, bestScore, dogsArr })
       copy[j] = temp;
     }
 
+    setShuffledCards([...copy]);
+  };
 
+  const markCard = (breed) => {
+    console.log(breed);
+    let copy = [...cardsToShowArr];
+    copy[copy.findIndex((dog) => dog.breed === breed)].clicked = true;
     setCardsToShowArr([...copy]);
-  }
+  };
 
   return (
     <>
       <ScoreBoard />
 
       <div className={`gameboard grid-${numberOfCards}`}>
-        
         {/* {areCardsReady() ? invokeCards() : <p>Loading dogs...</p>} */}
-        {cardsToShowArr ? invokeCards() : <p>Loading dogs...</p>}      
-
+        {shuffledCards ? invokeCards() : <p>Loading dogs...</p>}
       </div>
     </>
   );
